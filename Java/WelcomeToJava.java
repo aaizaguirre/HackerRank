0ffb9f9dbed34696cd22045bbf8dade51bd1a805
aaizaguirre <aaizaguirreo@gmail.com>
2023-06-09 09:54:16 -0500

    Documentacion WelcomeToJava

--- Java/WelcomeToJava.java
+++ Java/WelcomeToJava.java
@@ -1,3 +1,10 @@
+// Welcome to Java!
+// Welcome to the world of Java! In this challenge, we practice printing to stdout.
+// Input format: There is no input for this challenge.
+// Output format: You must print two lines of output:
+//      Print Hello, World. on the first line.
+//      Print Hello, Java. on the second line.
+
 public class WelcomeToJava {
     public static void main(String[] args){
         System.out.println("Hello, World.");
